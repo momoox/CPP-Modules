85d6e9100627859a87ec33405b703800d8f7e01d
Momox <morgane.geisler@gmail.com>
2024-04-13 19:56:32 +0200

    mod08 ex00 done

---
+++ Module 08/ex 00/easyFind.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyFind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgeisler <mgeisler@student.42mulhouse.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/07 18:31:38 by mgeisler          #+#    #+#             */
+/*   Updated: 2024/04/13 19:09:20 by mgeisler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <algorithm>
+# include <iostream>
+# include <vector>
+# include <set>
+# include <map>
+# include <string>
+# include <list>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::exception();
+	return it;
+}
+
+#endif
---
+++ Module 08/ex 00/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgeisler <mgeisler@student.42mulhouse.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/07 18:31:42 by mgeisler          #+#    #+#             */
+/*   Updated: 2024/04/13 19:56:19 by mgeisler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyFind.hpp"
+
+int main()
+{
+	std::vector<int> vec;
+	std::set<int> set;
+	std::list<int> list;
+
+	vec.push_back(2);
+	vec.push_back(5);
+	vec.push_back(6);
+
+	set.insert(3);
+	set.insert(5);
+	set.insert(1);
+	set.insert(7);
+
+	list.push_back(6);
+	list.push_back(8);
+
+	try {
+		std::cout << "Vector returns: " << *(easyfind(vec, 5)) << std::endl;
+		std::cout << "Set returns: " << *(easyfind(set, 5)) << std::endl;
+		std::cout << "List returns: " << *(easyfind(list, 5)) << std::endl;
+	}
+
+	catch (std::exception &e) {
+		std::cout << "no occurences found." << std::endl;
+	}
+	return (0);
+}
