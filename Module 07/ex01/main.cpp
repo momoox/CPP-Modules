894101be3b8c9052b8c0915acd7efc4ecdeb7aaa
momox <morgane.geisler@gmail.com>
2025-02-05 14:13:47 +0100

    tests main ex01 mod07

--- Module 07/ex01/main.cpp
+++ Module 07/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgeisler <mgeisler@student.42mulhouse.f    +#+  +:+       +#+        */
+/*   By: mgeisler <mgeisler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 12:03:05 by mgeisler          #+#    #+#             */
-/*   Updated: 2024/10/17 17:09:10 by mgeisler         ###   ########.fr       */
+/*   Updated: 2025/02/05 14:11:23 by mgeisler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,19 @@ int main(void)
 {
     int array[] = {1, 2, 3, 4, 5};
 
+    std::cout << "Array print: " << std::endl;
+
     iter(array, 5, print<int>);
 
     const int array2[] = {1, 2, 3, 4, 5};
 
+    std::cout << "Const array print: " << std::endl;
+
     iter(array2, 5, print<const int>);
 
     std::string strings[] = {"s1", "s2", "s3"};
+
+    std::cout << "String print: " << std::endl;
 
     iter(strings, 3, print<std::string>);
 }
