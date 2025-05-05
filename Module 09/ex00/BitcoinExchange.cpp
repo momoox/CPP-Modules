ba3fd63fae1ad1b6f8503c8e62d2d60d5a23e425
momox <morgane.geisler@gmail.com>
2025-05-05 21:37:10 +0200

    correction en cours cpp09

--- Module 09/ex00/BitcoinExchange.cpp
+++ Module 09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: mgeisler <mgeisler@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 19:51:59 by edelarbr          #+#    #+#             */
-/*   Updated: 2025/04/25 19:14:28 by mgeisler         ###   ########.fr       */
+/*   Updated: 2025/05/05 21:18:00 by mgeisler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,11 @@ BitcoinExchange &BitcoinExchange::operator = (const BitcoinExchange &rhs)
 	return (*this);
 }
 
-std::ifstream	BitcoinExchange::_FirstCheck(std::string input) {
+void	BitcoinExchange::_FirstCheck(std::string input, std::ifstream &inputFile) {
 	if (input.substr(input.length() - 4, 4) != ".txt" && input.substr(input.length() - 4, 4) != ".csv")
 		throw std::invalid_argument((std::string("Error: wrong input name, use input.txt or input.csv")));
 	
-	std::ifstream inputFile(input);
+	inputFile.open(input.c_str());
 	if (!inputFile)
 		throw std::invalid_argument(std::string("Error: input file can't be opened"));
 
@@ -42,37 +42,69 @@ std::ifstream	BitcoinExchange::_FirstCheck(std::string input) {
 	if(std::getline(inputFile, line))
 		if(line != "date | value")
 			std::cout << "Error: invalid header in input file, please write 'date | value'." << std::endl;
-	return(inputFile);
+	// return(inputFile);
 }
 
-int		BitcoinExchange::_InputFileIsValid(std::string current) {
-	
-		int	checker = 0;
-		int year = std::atoi(current.substr(0, 4).c_str());
-		int month = std::atoi(current.substr(5, 2).c_str());
-		int day = std::atoi(current.substr(8, 2).c_str());
-		std::string divider = current.substr(10, 3);
-		if(year < 2009 || year > 2022 || day < 1 || day > 31 || month < 1 || month > 12 || divider != " | ")
-		{
-			checker = 1;
-			std::cout << "Error: " << current.substr(0, 10) << " => wrong input." << std::endl;
-			return(checker);
-		}
-		
-		std::istringstream extract(current.substr(12));
-		double value;
-		extract >> value;
-		if(extract.fail() || !extract.eof() || value < 0)
-		{
-			checker = 1;
-			std::cout << "Error: " << current.substr(0, 9) << " => not a positive number." << std::endl;
-		}
-		else if(extract.fail() || !extract.eof() || value > 1000)
-		{
-			checker = 1;
-			std::cout << "Error: " << current << " => number too large." << std::endl;
-		}
-		return(checker);
+int	BitcoinExchange::_InputFileIsValid(std::string current) {
+    
+    // Ignorer la ligne d'en-tête pour éviter les erreurs
+    if (current == "date | value")
+        return 1;
+        
+    int	checker = 0;
+    
+    // Vérifier que la ligne a au moins la longueur minimale requise
+    if (current.length() < 13) {
+        std::cout << "Error: bad input => " << current << std::endl;
+        return 1;
+    }
+    
+    int year = atoi(current.substr(0, 4).c_str());
+    int month = atoi(current.substr(5, 2).c_str());
+    int day = atoi(current.substr(8, 2).c_str());
+    std::string divider = current.substr(10, 3);
+    
+    // Vérifier le format de date YYYY-MM-DD | VALUE
+    if (current[4] != '-' || current[7] != '-') {
+        std::cout << "Error: bad input format => " << current << std::endl;
+        return 1;
+    }
+    
+    if(year < 2009 || year > 2022 || day < 1 || day > 31 || month < 1 || month > 12 || divider != " | ")
+    {
+        checker = 1;
+        std::cout << "Error: bad input => " << current << std::endl;
+        return checker;
+    }
+    
+    // Vérifier les jours selon les mois
+    if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+        std::cout << "Error: bad input => " << current << std::endl;
+        return 1;
+    }
+    // Février
+    if (month == 2) {
+        bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        if ((isLeapYear && day > 29) || (!isLeapYear && day > 28)) {
+            std::cout << "Error: bad input => " << current << std::endl;
+            return 1;
+        }
+    }
+        
+    std::istringstream extract(current.substr(13)); // Changé de 12 à 13 pour ignorer l'espace après |
+    double value;
+    extract >> value;
+    if(extract.fail() || !extract.eof() || value < 0)
+    {
+        checker = 1;
+        std::cout << "Error: not a positive number." << std::endl;
+    }
+    else if(value > 1000)
+    {
+        checker = 1;
+        std::cout << "Error: number too large." << std::endl;
+    }
+    return checker;
 }
 
 float	BitcoinExchange::stringToFloat(std::string value) {
@@ -156,16 +188,18 @@ void	BitcoinExchange::_Operation(std::string input) {
 
 void	BitcoinExchange::change(std::string input, std::string db)
 {
-	std::ifstream inputFile(_FirstCheck(input));
-	std::ifstream dataBaseFile(db);
+	std::ifstream inputFile;
+	_FirstCheck(input, inputFile);
+    std::ifstream dataBaseFile(db.c_str());
 
 	std::string line;
-    while (std::getline(inputFile, line))
+    while (std::getline(inputFile, line)) {
         this->_inputVec.push_back(line);
+	}
 	
-	while(std::getline(dataBaseFile, line))
+	while(std::getline(dataBaseFile, line)) {
 		this->_dataBaseVec.push_back(line);
-
+	}
 	
 	std::vector<std::string>::iterator it;
 	for (it = this->_inputVec.begin(); it != this->_inputVec.end(); ++it) {
